acd9eea804d03719fb2027e2414e8dfdd0dd53dc
Garrett <garrett@coder.com>
2020-12-15 20:17:10 +0000

    Move install steps around

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# zsh
+yes | sudo pacman -S zsh
+if [ ! -d "$HOME/c" ] ; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	sudo chsh -s $(readlink -f $(which zsh)) $USER
+fi
+
+# dotfiles
 DOTFILES_CLONE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for dotfile in "$DOTFILES_CLONE_PATH/".*; do
@@ -14,10 +22,7 @@ for dotfile in "$DOTFILES_CLONE_PATH/".*; do
   ln -sf "$dotfile" "$HOME"
 done
 
-yes | sudo pacman -S zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo chsh -s $(readlink -f $(which zsh)) $USER
-
+# git
 if [ ! -d "$HOME/c" ] ; then
     git clone git@github.com:cdr/c.git "$HOME/c"
 fi
