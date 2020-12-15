699beacfa28c26168e698bb32a052593424d503e
Garrett <garrett@coder.com>
2020-12-15 17:01:47 +0000

    move logic to install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DOTFILES_CLONE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for dotfile in "$DOTFILES_CLONE_PATH/".*; do
+  # Skip `..` and '.'
+  [[ $dotfile =~ \.{1,2}$ ]] && continue
+  # Skip Git related
+  [[ $dotfile =~ \.git$ ]] && continue
+  [[ $dotfile =~ \.gitignore$ ]] && continue
+  [[ $dotfile =~ \.gitattributes$ ]] && continue
+
+  echo "Symlinking $dotfile"
+  ln -sf "$dotfile" "$HOME"
+done
+
+
+yes | sudo pacman -S zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo chsh -s $(readlink -f $(which zsh)) $USER
